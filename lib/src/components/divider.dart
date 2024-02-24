39d266d90eb99c764cb32026501551fe82827682
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-02-24 09:55:11 +0700

    Update divider to have an in-between child

--- lib/src/components/divider.dart
+++ lib/src/components/divider.dart
@@ -8,6 +8,8 @@ class Divider extends StatelessWidget {
   final double? thickness;
   final double? indent;
   final double? endIndent;
+  final Widget? child;
+  final EdgeInsets? padding;
 
   const Divider({
     Key? key,
@@ -16,11 +18,37 @@ class Divider extends StatelessWidget {
     this.thickness,
     this.indent,
     this.endIndent,
+    this.child,
+    this.padding = const EdgeInsets.symmetric(horizontal: 8),
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    if (child != null) {
+      return IntrinsicWidth(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: Container(
+                height: height ?? 1,
+                color: color ?? theme.colorScheme.border,
+                margin: EdgeInsets.only(left: indent ?? 0),
+              ),
+            ),
+            child!.muted().small().withPadding(padding: padding),
+            Expanded(
+              child: Container(
+                height: height ?? 1,
+                color: color ?? theme.colorScheme.border,
+                margin: EdgeInsets.only(right: endIndent ?? 0),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
     return AnimatedContainer(
       duration: kDefaultDuration,
       height: height ?? 1,
@@ -36,6 +64,8 @@ class VerticalDivider extends StatelessWidget {
   final double? thickness;
   final double? indent;
   final double? endIndent;
+  final Widget? child;
+  final EdgeInsets? padding;
 
   const VerticalDivider({
     Key? key,
@@ -44,11 +74,37 @@ class VerticalDivider extends StatelessWidget {
     this.thickness,
     this.indent,
     this.endIndent,
+    this.child,
+    this.padding = const EdgeInsets.symmetric(vertical: 8),
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    if (child != null) {
+      return IntrinsicHeight(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: Container(
+                width: width ?? 1,
+                color: color ?? theme.colorScheme.border,
+                margin: EdgeInsets.only(top: indent ?? 0),
+              ),
+            ),
+            child!.muted().small().withPadding(padding: padding),
+            Expanded(
+              child: Container(
+                width: width ?? 1,
+                color: color ?? theme.colorScheme.border,
+                margin: EdgeInsets.only(bottom: endIndent ?? 0),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
     return AnimatedContainer(
       duration: kDefaultDuration,
       width: width ?? 1,
