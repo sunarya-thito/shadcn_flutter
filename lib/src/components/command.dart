228a8e4f0d92d8d5574b07feb8dfb9a34684f73a
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-02-24 09:56:33 +0700

    Added command widget

---
+++ lib/src/components/command.dart
@@ -0,0 +1,292 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart';
+
+typedef CommandBuilder = Stream<List<Widget>> Function(
+    BuildContext context, String? query);
+
+typedef ErrorWidgetBuilder = Widget Function(
+    BuildContext context, Object error, StackTrace? stackTrace);
+
+class CommandEmpty extends StatelessWidget {
+  const CommandEmpty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child:
+            const Text('No results found.').withPadding(vertical: 24).small());
+  }
+}
+
+class Command extends StatefulWidget {
+  final CommandBuilder builder;
+  final Duration
+      debounceDuration; // debounce is used to prevent too many requests
+  final WidgetBuilder? emptyBuilder;
+  final ErrorWidgetBuilder? errorBuilder;
+  final WidgetBuilder? loadingBuilder;
+
+  const Command({
+    Key? key,
+    required this.builder,
+    this.debounceDuration = const Duration(milliseconds: 300),
+    this.emptyBuilder,
+    this.errorBuilder,
+    this.loadingBuilder,
+  }) : super(key: key);
+
+  @override
+  State<Command> createState() => _CommandState();
+}
+
+class _CommandState extends State<Command> {
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode _textFieldFocus = FocusNode();
+  final FocusScopeNode _focusScopeNode = FocusScopeNode();
+
+  late Stream<List<Widget>> _currentRequest;
+
+  int requestCount = 0;
+
+  Stream<List<Widget>> _request(BuildContext context) async* {
+    int currentRequest = ++requestCount;
+    yield [];
+    await Future.delayed(widget.debounceDuration);
+    if (!context.mounted || currentRequest != requestCount) return;
+    List<Widget> resultItems = [];
+    await for (final items in widget.builder(context, _controller.text)) {
+      if (currentRequest != requestCount) continue;
+      // append items
+      resultItems.addAll(items);
+      yield resultItems;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _currentRequest = _request(context);
+    _controller.addListener(() {
+      setState(() {
+        _currentRequest = _request(context);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    bool canPop = Navigator.of(context).canPop();
+    return IntrinsicWidth(
+      child: OutlinedContainer(
+        clipBehavior: Clip.hardEdge,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.search,
+                  size: 16,
+                  color: theme.colorScheme.mutedForeground,
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    border: false,
+                    focusNode: _textFieldFocus,
+                  ),
+                ),
+                if (canPop)
+                  Button(
+                    type: ButtonType.dense,
+                    size: ButtonSize.none,
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Icon(
+                      Icons.close,
+                      size: 16,
+                    ),
+                  ),
+              ],
+            ).withPadding(horizontal: 12),
+            const Divider(),
+            Expanded(
+              child: FocusScope(
+                node: _focusScopeNode,
+                parentNode: _textFieldFocus,
+                skipTraversal: true,
+                child: StreamBuilder(
+                  stream: _currentRequest,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      List<Widget> items = List.of(snapshot.data!);
+                      if (snapshot.connectionState == ConnectionState.active) {
+                        items.add(
+                            const Center(child: CircularProgressIndicator())
+                                .withPadding(vertical: 24));
+                      } else if (items.isEmpty) {
+                        return widget.emptyBuilder?.call(context) ??
+                            const CommandEmpty();
+                      }
+                      return ListView.separated(
+                        separatorBuilder: (context, index) => const Divider(),
+                        shrinkWrap: true,
+                        itemCount: items.length,
+                        itemBuilder: (context, index) => items[index],
+                      );
+                    }
+                    return widget.loadingBuilder?.call(context) ??
+                        const Center(child: CircularProgressIndicator())
+                            .withPadding(vertical: 24);
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CommandCategory extends StatelessWidget {
+  final List<Widget> children;
+  final Widget? title;
+
+  const CommandCategory({
+    Key? key,
+    required this.children,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (title != null)
+          title!
+              .withPadding(horizontal: 8, vertical: 6)
+              .medium()
+              .xSmall()
+              .muted(),
+        ...children,
+      ],
+    ).withPadding(all: 4);
+  }
+}
+
+class CommandItem extends StatefulWidget {
+  final Widget? leading;
+  final Widget title;
+  final Widget? trailing;
+  final VoidCallback? onTap;
+
+  const CommandItem({
+    Key? key,
+    this.leading,
+    required this.title,
+    this.trailing,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  State<CommandItem> createState() => _CommandItemState();
+}
+
+class _CommandItemState extends State<CommandItem> {
+  final FocusNode _focusNode = FocusNode();
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var themeData = Theme.of(context);
+    return GestureDetector(
+      onTap: widget.onTap,
+      child: FocusableActionDetector(
+        autofocus: true,
+        enabled: widget.onTap != null,
+        mouseCursor: SystemMouseCursors.click,
+        focusNode: _focusNode,
+        onShowHoverHighlight: (value) {
+          if (value && widget.onTap != null) {
+            if (!_focusNode.hasFocus) {
+              _focusNode.requestFocus();
+            }
+          }
+        },
+        actions: {
+          ActivateIntent: CallbackAction(
+            onInvoke: (e) {
+              if (widget.onTap != null) {
+                widget.onTap!();
+                return true;
+              }
+              return false;
+            },
+          ),
+        },
+        shortcuts: {
+          LogicalKeySet(LogicalKeyboardKey.enter): const ActivateIntent(),
+          LogicalKeySet(LogicalKeyboardKey.arrowUp):
+              const DirectionalFocusIntent(TraversalDirection.up),
+          LogicalKeySet(LogicalKeyboardKey.arrowDown):
+              const DirectionalFocusIntent(TraversalDirection.down),
+          LogicalKeySet(LogicalKeyboardKey.arrowLeft):
+              const DirectionalFocusIntent(TraversalDirection.left),
+          LogicalKeySet(LogicalKeyboardKey.arrowRight):
+              const DirectionalFocusIntent(TraversalDirection.right),
+        },
+        child: AnimatedContainer(
+          duration: kDefaultDuration,
+          decoration: BoxDecoration(
+            color: _focusNode.hasFocus
+                ? themeData.colorScheme.accent
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(themeData.radiusSm),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+          child: AnimatedIconTheme(
+            duration: kDefaultDuration,
+            data: IconThemeData(
+              size: 15,
+              color: widget.onTap != null
+                  ? themeData.colorScheme.accentForeground
+                  : themeData.colorScheme.accentForeground.withOpacity(0.5),
+            ),
+            child: AnimatedDefaultTextStyle(
+              style: TextStyle(
+                color: widget.onTap != null
+                    ? themeData.colorScheme.accentForeground
+                    : themeData.colorScheme.accentForeground.withOpacity(0.5),
+              ),
+              duration: kDefaultDuration,
+              child: Row(
+                children: [
+                  if (widget.leading != null) widget.leading!,
+                  if (widget.leading != null) gap(8),
+                  Expanded(child: widget.title),
+                  if (widget.trailing != null) gap(8),
+                  if (widget.trailing != null)
+                    widget.trailing!.muted().xSmall(),
+                ],
+              ).small(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
