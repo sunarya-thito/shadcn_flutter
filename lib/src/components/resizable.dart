40e6f1c0489e04a8c35caf275861c5146924ea08
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-07-09 17:44:13 +0700

    Mark resizable as WIP

--- lib/src/components/resizable.dart
+++ lib/src/components/resizable.dart
@@ -1,8 +1,7 @@
-import 'package:shadcn_flutter/shadcn_flutter.dart';
-
-class ResizablePanel extends StatelessWidget {
-  final List<Widget> children;
-  final AxisDirection direction;
-  final WidgetBuilder? dividerBuilder;
-  final double dividerSize;
-}
+// WIP
+// class ResizablePanel extends StatelessWidget {
+//   final List<Widget> children;
+//   final AxisDirection direction;
+//   final WidgetBuilder? dividerBuilder;
+//   final double dividerSize;
+// }
