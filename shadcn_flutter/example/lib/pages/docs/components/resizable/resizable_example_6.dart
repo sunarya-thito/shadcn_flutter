bf9e013e0cc8584108a988be8f47d14b483ddf95
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-07-16 16:48:26 +0700

    feat(resizable): Add nested example
    This commit adds a new example to demonstrate nested resizable panels.

--- shadcn_flutter/example/lib/pages/docs/components/resizable/resizable_example_6.dart
+++ shadcn_flutter/example/lib/pages/docs/components/resizable/resizable_example_6.dart
@@ -1,42 +1,69 @@
 import 'package:example/pages/docs/components/carousel_example.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
-class ResizableExample2 extends StatefulWidget {
+class ResizableExample6 extends StatefulWidget {
   @override
-  State<ResizableExample2> createState() => _ResizableExample2State();
+  State<ResizableExample6> createState() => _ResizableExample6State();
 }
 
-class _ResizableExample2State extends State<ResizableExample2> {
+class _ResizableExample6State extends State<ResizableExample6> {
   @override
   Widget build(BuildContext context) {
     return OutlinedContainer(
       clipBehavior: Clip.antiAlias,
       child: ResizablePanel(
-        direction: Axis.vertical,
+        direction: Axis.horizontal,
         children: [
           ResizablePane(
             child: NumberedContainer(
               index: 0,
-              width: 200,
+              height: 200,
               fill: false,
             ),
-            initialSize: 80,
+            initialSize: 100,
+          ),
+          ResizablePane(
+            child: ResizablePanel(
+              direction: Axis.vertical,
+              children: [
+                ResizablePane(
+                  child: NumberedContainer(
+                    index: 1,
+                    fill: false,
+                  ),
+                  initialSize: 80,
+                ),
+                ResizablePane(
+                  child: ResizablePanel(
+                    direction: Axis.horizontal,
+                    children: [
+                      ResizablePane.flex(
+                        child: NumberedContainer(
+                          index: 2,
+                          fill: false,
+                        ),
+                      ),
+                      ResizablePane.flex(
+                        child: NumberedContainer(
+                          index: 3,
+                          fill: false,
+                        ),
+                      ),
+                    ],
+                  ),
+                  initialSize: 120,
+                ),
+              ],
+            ),
+            initialSize: 300,
           ),
           ResizablePane(
             child: NumberedContainer(
-              index: 1,
-              width: 200,
+              index: 4,
+              height: 200,
               fill: false,
             ),
-            initialSize: 120,
-          ),
-          ResizablePane(
-            child: NumberedContainer(
-              index: 2,
-              width: 200,
-              fill: false,
-            ),
-            initialSize: 80,
+            initialSize: 100,
           ),
         ],
       ),
