a19b30e6fa62c4b15861a624e54d3c5bf28c190d
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-07-17 23:37:54 +0700

    Fix: Use normal mode for components page.

--- shadcn_flutter/example/lib/pages/docs/components_page.dart
+++ shadcn_flutter/example/lib/pages/docs/components_page.dart
@@ -47,7 +47,7 @@ class _ComponentsPageState extends State<ComponentsPage> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Data(
-      data: ComponentsMode.capture,
+      data: ComponentsMode.normal,
       child: DocsPage(
         name: 'components',
         onThisPage: {
