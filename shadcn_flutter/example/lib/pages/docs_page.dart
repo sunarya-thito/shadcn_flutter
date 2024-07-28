94793ba0133eee100b06a12de826049e4ffc4d27
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-07-29 01:19:41 +0700

    Marked sortable as WIP in docs

--- shadcn_flutter/example/lib/pages/docs_page.dart
+++ shadcn_flutter/example/lib/pages/docs_page.dart
@@ -218,7 +218,7 @@ class DocsPageState extends State<DocsPage> {
         ShadcnDocsPage('Divider', 'divider'),
         ShadcnDocsPage('Resizable', 'resizable'),
         // https://nextjs-shadcn-dnd.vercel.app/ (make it headless)
-        ShadcnDocsPage('Sortable', 'sortable'),
+        ShadcnDocsPage('Sortable', 'sortable', ShadcnFeatureTag.workInProgress),
         ShadcnDocsPage('Steps', 'steps'),
         ShadcnDocsPage('Stepper', 'stepper'),
         ShadcnDocsPage('Timeline', 'timeline'),
