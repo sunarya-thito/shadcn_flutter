ac272ab60de830a9e19d27f8e2db0d38002bdd4a
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-02 21:07:41 +0700

    Updated context_menu_example_1.dart

--- shadcn_flutter/example/lib/pages/docs/components/context_menu/context_menu_example_1.dart
+++ shadcn_flutter/example/lib/pages/docs/components/context_menu/context_menu_example_1.dart
@@ -16,120 +16,119 @@ class _ContextMenuExample1State extends State<ContextMenuExample1> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return ContextMenu(
-      items: [
-        const MenuButton(
-          trailing: MenuShortcut(
-            activator: SingleActivator(
-              LogicalKeyboardKey.bracketLeft,
-              control: true,
-            ),
-          ),
-          child: Text('Back'),
-        ),
-        const MenuButton(
-          trailing: MenuShortcut(
-            activator: SingleActivator(
-              LogicalKeyboardKey.bracketRight,
-              control: true,
-            ),
-          ),
-          enabled: false,
-          child: Text('Forward'),
-        ),
-        const MenuButton(
-          trailing: MenuShortcut(
-            activator: SingleActivator(
-              LogicalKeyboardKey.keyR,
-              control: true,
-            ),
-          ),
-          child: Text('Reload'),
-        ),
-        const MenuButton(
-          subMenu: [
-            MenuButton(
-              trailing: MenuShortcut(
-                activator: SingleActivator(
-                  LogicalKeyboardKey.keyS,
-                  control: true,
-                ),
+        items: [
+          const MenuButton(
+            trailing: MenuShortcut(
+              activator: SingleActivator(
+                LogicalKeyboardKey.bracketLeft,
+                control: true,
               ),
-              child: Text('Save Page As...'),
             ),
-            MenuButton(
-              child: Text('Create Shortcut...'),
-            ),
-            MenuButton(
-              child: Text('Name Window...'),
-            ),
-            MenuDivider(),
-            MenuButton(
-              child: Text('Developer Tools'),
-            ),
-          ],
-          child: Text('More Tools'),
-        ),
-        const MenuDivider(),
-        MenuCheckbox(
-          value: showBookmarksBar,
-          onChanged: (context, value) {
-            setState(() {
-              showBookmarksBar = value;
-            });
-          },
-          autoClose: false,
-          trailing: const MenuShortcut(
-            activator: SingleActivator(
-              LogicalKeyboardKey.keyB,
-              control: true,
-              shift: true,
-            ),
+            child: Text('Back'),
           ),
-          child: const Text('Show Bookmarks Bar'),
-        ),
-        MenuCheckbox(
-          value: showFullUrls,
-          onChanged: (context, value) {
-            setState(() {
-              showFullUrls = value;
-            });
-          },
-          autoClose: false,
-          child: const Text('Show Full URLs'),
-        ),
-        const MenuDivider(),
-        const MenuLabel(child: Text('People')),
-        const MenuDivider(),
-        MenuRadioGroup(
-          value: people,
-          onChanged: (context, value) {
-            setState(() {
-              people = value;
-            });
-          },
-          children: const [
-            MenuRadio(
-              value: 0,
-              autoClose: false,
-              child: Text('Pedro Duarte'),
+          const MenuButton(
+            trailing: MenuShortcut(
+              activator: SingleActivator(
+                LogicalKeyboardKey.bracketRight,
+                control: true,
+              ),
             ),
-            MenuRadio(
-              value: 1,
-              autoClose: false,
-              child: Text('Colm Tuite'),
+            enabled: false,
+            child: Text('Forward'),
+          ),
+          const MenuButton(
+            trailing: MenuShortcut(
+              activator: SingleActivator(
+                LogicalKeyboardKey.keyR,
+                control: true,
+              ),
             ),
-          ],
-        ),
-      ],
-      child: DashedContainer(
-        borderRadius: BorderRadius.circular(theme.radiusMd),
-        width: 2,
-        gap: 2,
-        child: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 172, vertical: 148),
-          child: Text('Right click here'),
-        ),
-      ),
-    );
+            child: Text('Reload'),
+          ),
+          const MenuButton(
+            subMenu: [
+              MenuButton(
+                trailing: MenuShortcut(
+                  activator: SingleActivator(
+                    LogicalKeyboardKey.keyS,
+                    control: true,
+                  ),
+                ),
+                child: Text('Save Page As...'),
+              ),
+              MenuButton(
+                child: Text('Create Shortcut...'),
+              ),
+              MenuButton(
+                child: Text('Name Window...'),
+              ),
+              MenuDivider(),
+              MenuButton(
+                child: Text('Developer Tools'),
+              ),
+            ],
+            child: Text('More Tools'),
+          ),
+          const MenuDivider(),
+          MenuCheckbox(
+            value: showBookmarksBar,
+            onChanged: (context, value) {
+              setState(() {
+                showBookmarksBar = value;
+              });
+            },
+            autoClose: false,
+            trailing: const MenuShortcut(
+              activator: SingleActivator(
+                LogicalKeyboardKey.keyB,
+                control: true,
+                shift: true,
+              ),
+            ),
+            child: const Text('Show Bookmarks Bar'),
+          ),
+          MenuCheckbox(
+            value: showFullUrls,
+            onChanged: (context, value) {
+              setState(() {
+                showFullUrls = value;
+              });
+            },
+            autoClose: false,
+            child: const Text('Show Full URLs'),
+          ),
+          const MenuDivider(),
+          const MenuLabel(child: Text('People')),
+          const MenuDivider(),
+          MenuRadioGroup(
+            value: people,
+            onChanged: (context, value) {
+              setState(() {
+                people = value;
+              });
+            },
+            children: const [
+              MenuRadio(
+                value: 0,
+                autoClose: false,
+                child: Text('Pedro Duarte'),
+              ),
+              MenuRadio(
+                value: 1,
+                autoClose: false,
+                child: Text('Colm Tuite'),
+              ),
+            ],
+          ),
+        ],
+        child: DashedContainer(
+          borderRadius: BorderRadius.circular(theme.radiusMd),
+          strokeWidth: 2,
+          gap: 2,
+          child: Text('Right click here').center(),
+        ).constrained(
+          maxWidth: 300,
+          maxHeight: 200,
+        ));
   }
 }
