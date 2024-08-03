89a7c5ecda44146d2032a387b965bc24a82f64d1
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-04 06:00:18 +0700

    Update linear progress indicator

--- shadcn_flutter/lib/src/components/display/linear_progress_indicator.dart
+++ shadcn_flutter/lib/src/components/display/linear_progress_indicator.dart
@@ -1,15 +1,37 @@
-import 'package:flutter/material.dart' as m;
+import 'dart:ui' as ui;
+
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
-// delegates from LinearProgressIndicator (material) to a custom styled LinearProgressIndicator
+const int _kIndeterminateLinearDuration = 1800;
+
 class LinearProgressIndicator extends StatelessWidget {
+  static const Curve _line1Head = Interval(
+    0.0,
+    750.0 / _kIndeterminateLinearDuration,
+    curve: Cubic(0.2, 0.0, 0.8, 1.0),
+  );
+  static const Curve _line1Tail = Interval(
+    333.0 / _kIndeterminateLinearDuration,
+    (333.0 + 750.0) / _kIndeterminateLinearDuration,
+    curve: Cubic(0.4, 0.0, 1.0, 1.0),
+  );
+  static const Curve _line2Head = Interval(
+    1000.0 / _kIndeterminateLinearDuration,
+    (1000.0 + 567.0) / _kIndeterminateLinearDuration,
+    curve: Cubic(0.0, 0.0, 0.65, 1.0),
+  );
+  static const Curve _line2Tail = Interval(
+    1267.0 / _kIndeterminateLinearDuration,
+    (1267.0 + 533.0) / _kIndeterminateLinearDuration,
+    curve: Cubic(0.10, 0.0, 0.45, 1.0),
+  );
+
   final double? value;
   final Color? backgroundColor;
   final double? minHeight;
   final Color? color;
   final BorderRadius? borderRadius;
-  final String? semanticsLabel;
-  final String? semanticsValue;
+  final bool showSparks;
 
   const LinearProgressIndicator({
     Key? key,
@@ -18,21 +40,193 @@ class LinearProgressIndicator extends StatelessWidget {
     this.minHeight,
     this.color,
     this.borderRadius,
-    this.semanticsLabel,
-    this.semanticsValue,
+    this.showSparks = true,
   }) : super(key: key);
 
   @override
-  Widget build(m.BuildContext context) {
+  Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return m.LinearProgressIndicator(
-      value: value,
-      backgroundColor:
-          backgroundColor ?? theme.colorScheme.primary.withOpacity(0.2),
-      minHeight: minHeight ?? 4,
-      color: color ?? theme.colorScheme.primary,
-      semanticsLabel: semanticsLabel,
-      semanticsValue: semanticsValue,
+    final directionality = Directionality.of(context);
+    Widget childWidget;
+    if (value != null) {
+      childWidget = AnimatedValueBuilder(
+          value: value,
+          duration: kDefaultDuration,
+          curve: Curves.easeInOut,
+          builder: (context, value, child) {
+            return CustomPaint(
+              painter: _LinearProgressIndicatorPainter(
+                start: 0,
+                end: value ?? 0,
+                color: color ?? theme.colorScheme.primary,
+                backgroundColor: backgroundColor ??
+                    theme.colorScheme.primary.withOpacity(0.2),
+                showSparks: showSparks,
+                sparksColor: color ?? theme.colorScheme.primary,
+                sparksRadius: 15,
+                textDirection: directionality,
+              ),
+            );
+          });
+    } else {
+      // indeterminate
+      childWidget = RepeatedAnimationBuilder(
+        start: 0.0,
+        end: 1.0,
+        duration: const Duration(milliseconds: _kIndeterminateLinearDuration),
+        builder: (context, value, child) {
+          double start = _line1Tail.transform(value);
+          double end = _line1Head.transform(value);
+          double start2 = _line2Tail.transform(value);
+          double end2 = _line2Head.transform(value);
+          return CustomPaint(
+            painter: _LinearProgressIndicatorPainter(
+              start: start,
+              end: end,
+              start2: start2,
+              end2: end2,
+              color: color ?? theme.colorScheme.primary,
+              backgroundColor:
+                  backgroundColor ?? theme.colorScheme.primary.withOpacity(0.2),
+              showSparks: false,
+              sparksColor: Colors.transparent,
+              sparksRadius: 0,
+              textDirection: directionality,
+            ),
+          );
+        },
+      );
+    }
+    return RepaintBoundary(
+      child: SizedBox(
+        height: minHeight ?? 2,
+        child: childWidget,
+      ),
     );
+  }
+}
+
+class _LinearProgressIndicatorPainter extends CustomPainter {
+  static final gradientTransform =
+      (Matrix4.identity()..scale(1.0, 0.5)).storage;
+
+  final double start;
+  final double end;
+  final double? start2; // for indeterminate
+  final double? end2;
+  final Color color;
+  final Color backgroundColor;
+  final bool showSparks;
+  final Color sparksColor;
+  final double sparksRadius;
+  final TextDirection textDirection;
+
+  _LinearProgressIndicatorPainter({
+    required this.start,
+    required this.end,
+    this.start2,
+    this.end2,
+    required this.color,
+    required this.backgroundColor,
+    required this.showSparks,
+    required this.sparksColor,
+    required this.sparksRadius,
+    this.textDirection = TextDirection.ltr,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var start = this.start;
+    var end = this.end;
+    var start2 = this.start2;
+    var end2 = this.end2;
+    if (textDirection == TextDirection.rtl) {
+      start = 1 - end;
+      end = 1 - this.start;
+      if (start2 != null && end2 != null) {
+        start2 = 1 - end2;
+        end2 = 1 - this.start2!;
+      }
+    }
+
+    if (start.isNaN) {
+      start = 0;
+    }
+    if (end.isNaN) {
+      end = 0;
+    }
+    if (start2 != null && start2.isNaN) {
+      start2 = 0;
+    }
+    if (end2 != null && end2.isNaN) {
+      end2 = 0;
+    }
+
+    final paint = Paint()..style = PaintingStyle.fill;
+
+    paint.color = backgroundColor;
+
+    canvas.drawRRect(
+      RRect.fromLTRBR(
+          0, 0, size.width, size.height, Radius.circular(size.height / 2)),
+      paint,
+    );
+
+    paint.color = color;
+    var rectValue = Rect.fromLTWH(
+      size.width * start,
+      0,
+      size.width * (end - start),
+      size.height,
+    );
+    canvas.drawRect(rectValue, paint);
+    if (start2 != null && end2 != null) {
+      rectValue = Rect.fromLTWH(
+        size.width * start2,
+        0,
+        size.width * (end2 - start2),
+        size.height,
+      );
+      canvas.drawRect(rectValue, paint);
+    }
+
+    if (showSparks) {
+      // use RadialGradient to create sparks
+
+      final gradient = ui.Gradient.radial(
+        // colors: [sparksColor, Colors.transparent],
+        // stops: const [0.0, 1.0],
+        Offset(size.width * (end - start), size.height / 2),
+        sparksRadius,
+        [
+          sparksColor,
+          sparksColor.withAlpha(0),
+        ],
+        [0.0, 1.0],
+        ui.TileMode.clamp,
+        // scale to make oval
+        gradientTransform,
+      );
+      paint.shader = gradient;
+      canvas.drawCircle(
+        Offset(size.width * (end - start), size.height / 2),
+        sparksRadius,
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant _LinearProgressIndicatorPainter oldDelegate) {
+    return oldDelegate.start != start ||
+        oldDelegate.end != end ||
+        oldDelegate.color != color ||
+        oldDelegate.backgroundColor != backgroundColor ||
+        oldDelegate.showSparks != showSparks ||
+        oldDelegate.sparksColor != sparksColor ||
+        oldDelegate.sparksRadius != sparksRadius ||
+        oldDelegate.textDirection != textDirection ||
+        oldDelegate.start2 != start2 ||
+        oldDelegate.end2 != end2;
   }
 }
