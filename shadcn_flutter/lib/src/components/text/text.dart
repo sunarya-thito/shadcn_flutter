92bca96aaf07ac03e602dc0f54ac19424e558260
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-05 01:49:31 +0700

    Fix h2 animation property

--- shadcn_flutter/lib/src/components/text/text.dart
+++ shadcn_flutter/lib/src/components/text/text.dart
@@ -326,7 +326,8 @@ extension TextExtension on Widget {
         child = WrappedText(
             style: (context, theme) => theme.typography.h2, child: this);
       }
-      return Container(
+      return AnimatedContainer(
+        duration: kDefaultDuration,
         margin: const EdgeInsets.only(top: 40),
         padding: const EdgeInsets.only(bottom: 8),
         decoration: BoxDecoration(
