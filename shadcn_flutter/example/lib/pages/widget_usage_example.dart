a4c1404599865f8680154570cd5265c48f4cf8fd
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-06 14:34:54 +0700

    Capped code example height

--- shadcn_flutter/example/lib/pages/widget_usage_example.dart
+++ shadcn_flutter/example/lib/pages/widget_usage_example.dart
@@ -73,10 +73,15 @@ class _WidgetUsageExampleState extends State<WidgetUsageExample> {
         RepaintBoundary(
           child: Offstage(
             offstage: index != 1,
-            child: CodeSnippetFutureBuilder(
-              path: widget.path,
-              mode: 'dart',
-              summarize: widget.summarize,
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxHeight: 550,
+              ),
+              child: CodeSnippetFutureBuilder(
+                path: widget.path,
+                mode: 'dart',
+                summarize: widget.summarize,
+              ),
             ),
           ),
         )
