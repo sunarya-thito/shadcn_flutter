47fc3af0015afcc00371a60bc7ea3ab039b6fbf4
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-09 20:00:22 +0700

    Animated star rating

--- shadcn_flutter/lib/src/components/form/star_rating.dart
+++ shadcn_flutter/lib/src/components/form/star_rating.dart
@@ -72,7 +72,8 @@ class _StarRatingState extends State<StarRating> with FormValueSupplier {
     var starInnerRadiusRatio = widget.starInnerRadiusRatio ?? 0.4;
     var starRotation = widget.starRotation ?? 0.0;
     var starSize = widget.starSize ?? 24.0;
-    return Container(
+    return AnimatedContainer(
+      duration: kDefaultDuration,
       width: starSize * scaling,
       height: starSize * scaling,
       decoration: ShapeDecoration(
@@ -93,64 +94,71 @@ class _StarRatingState extends State<StarRating> with FormValueSupplier {
   Widget build(BuildContext context) {
     double roundedValue =
         ((_changingValue ?? widget.value) / widget.step).round() * widget.step;
-    final theme = Theme.of(context);
-    final scaling = theme.scaling;
-    var starSize = widget.starSize != null ? widget.starSize! : 24.0 * scaling;
-    var starSpacing =
-        widget.starSpacing != null ? widget.starSpacing! : 5.0 * scaling;
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      onTap: () {
-        if (widget.onChanged != null && roundedValue != widget.value) {
-          widget.onChanged!(roundedValue);
-        }
+    return AnimatedValueBuilder(
+      value: roundedValue,
+      duration: kDefaultDuration,
+      builder: (context, roundedValue, child) {
+        final theme = Theme.of(context);
+        final scaling = theme.scaling;
+        var starSize =
+            widget.starSize != null ? widget.starSize! : 24.0 * scaling;
+        var starSpacing =
+            widget.starSpacing != null ? widget.starSpacing! : 5.0 * scaling;
+        return GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () {
+            if (widget.onChanged != null && roundedValue != widget.value) {
+              widget.onChanged!(roundedValue);
+            }
+          },
+          child: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            onExit: (event) {
+              setState(() {
+                _changingValue = null;
+              });
+            },
+            child: ClipRect(
+              child: Flex(
+                direction: widget.direction,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  for (var i = 0; i < widget.max.ceil(); i++)
+                    MouseRegion(
+                      hitTestBehavior: HitTestBehavior.translucent,
+                      onHover: (event) {
+                        if (widget.onChanged == null) return;
+                        double progress =
+                            (event.localPosition.dx / starSize).clamp(0.0, 1.0);
+                        setState(() {
+                          _changingValue = (i + progress);
+                        });
+                      },
+                      child: ShaderMask(
+                        shaderCallback: (bounds) {
+                          return LinearGradient(
+                            colors: [
+                              widget.activeColor ??
+                                  Theme.of(context).colorScheme.primary,
+                              widget.backgroundColor ??
+                                  Theme.of(context).colorScheme.muted,
+                            ],
+                            stops: [
+                              (roundedValue - i).clamp(0.0, 1.0),
+                              (roundedValue - i).clamp(0.0, 1.0),
+                            ],
+                          ).createShader(bounds);
+                        },
+                        blendMode: BlendMode.srcIn,
+                        child: _buildStar(context),
+                      ),
+                    ),
+                ],
+              ).gap(starSpacing),
+            ),
+          ),
+        );
       },
-      child: MouseRegion(
-        cursor: SystemMouseCursors.click,
-        onExit: (event) {
-          setState(() {
-            _changingValue = null;
-          });
-        },
-        child: ClipRect(
-          child: Flex(
-            direction: widget.direction,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              for (var i = 0; i < widget.max.ceil(); i++)
-                MouseRegion(
-                  hitTestBehavior: HitTestBehavior.translucent,
-                  onHover: (event) {
-                    if (widget.onChanged == null) return;
-                    double progress =
-                        (event.localPosition.dx / starSize).clamp(0.0, 1.0);
-                    setState(() {
-                      _changingValue = (i + progress);
-                    });
-                  },
-                  child: ShaderMask(
-                    shaderCallback: (bounds) {
-                      return LinearGradient(
-                        colors: [
-                          widget.activeColor ??
-                              Theme.of(context).colorScheme.primary,
-                          widget.backgroundColor ??
-                              Theme.of(context).colorScheme.muted,
-                        ],
-                        stops: [
-                          (roundedValue - i).clamp(0.0, 1.0),
-                          (roundedValue - i).clamp(0.0, 1.0),
-                        ],
-                      ).createShader(bounds);
-                    },
-                    blendMode: BlendMode.srcIn,
-                    child: _buildStar(context),
-                  ),
-                ),
-            ],
-          ).gap(starSpacing),
-        ),
-      ),
     );
   }
 }
