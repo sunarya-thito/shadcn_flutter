2e2ccc080b6fafc69ed9df4230866c79490c500b
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-08-24 18:23:38 +0700

    Update refresh trigger

--- lib/src/components/overlay/refresh_trigger.dart
+++ lib/src/components/overlay/refresh_trigger.dart
@@ -62,146 +62,114 @@ class _RefreshTriggerState extends State<RefreshTrigger>
     with SingleTickerProviderStateMixin {
   double _currentExtent = 0;
   bool _scrolling = false;
-  ScrollableState? _scrollable;
   TriggerStage _stage = TriggerStage.idle;
 
   double _calculateSafeExtent(double extent) {
-    var maxExtent = widget.maxExtent;
-    if (maxExtent != null) {
-      if (extent >= widget.minExtent) {
-        double relativeExtent = extent - widget.minExtent;
-        double diff = maxExtent - relativeExtent;
-        return extent - diff / 2;
-      }
-    }
     return extent;
+  }
+
+  Widget _wrapPositioned(Widget child) {
+    if (widget.direction == Axis.vertical) {
+      return Positioned(
+        top: !widget.reverse ? 0 : null,
+        bottom: !widget.reverse ? null : 0,
+        left: 0,
+        right: 0,
+        child: child,
+      );
+    } else {
+      return Positioned(
+        top: 0,
+        bottom: 0,
+        left: !widget.reverse ? null : 0,
+        right: !widget.reverse ? 0 : null,
+        child: child,
+      );
+    }
+  }
+
+  Offset get _offset {
+    if (widget.direction == Axis.vertical) {
+      return Offset(0, widget.reverse ? 1 : -1);
+    } else {
+      return Offset(widget.reverse ? 1 : -1, 0);
+    }
+  }
+
+  bool _handleScrollNotification(ScrollNotification notification) {
+    if (notification is ScrollStartNotification) {
+      setState(() {
+        _scrolling = true;
+      });
+    }
+    return false;
+  }
+
+  bool _handleOverscrollNotification(
+      OverscrollIndicatorNotification notification) {
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
     return NotificationListener<ScrollNotification>(
-      onNotification: (notification) {
-        if (notification is ScrollStartNotification &&
-            notification.depth == 0 &&
-            _stage == TriggerStage.idle) {
-          var context = notification.context;
-          if (context != null) {
-            // hold the scroll position
-            ScrollableState? scrollable =
-                Scrollable.maybeOf(context, axis: widget.direction);
-            if (scrollable != null) {
-              setState(() {
-                _currentExtent = 0.0;
-                _scrolling = true;
-                _scrollable = scrollable;
-                _stage = TriggerStage.pulling;
-              });
-            }
-          }
-          return true;
-        }
-        if (notification is ScrollEndNotification &&
-            notification.depth == 0 &&
-            _stage == TriggerStage.pulling) {
-          setState(() {
-            double currentExtent = _currentExtent;
-            _scrolling = false;
-            _currentExtent = 0.0;
-            _scrollable = null;
-            if (currentExtent >= widget.minExtent) {
-              _stage = TriggerStage.refreshing;
-              widget.onRefresh?.call().then((_) {
-                if (mounted) {
-                  setState(() {
-                    _stage = TriggerStage.completed;
-                    Timer(Duration(seconds: 1), () {
-                      if (mounted) {
-                        setState(() {
-                          _stage = TriggerStage.idle;
-                        });
-                      }
-                    });
-                  });
-                }
-              });
-            } else {
-              _stage = TriggerStage.idle;
-            }
-          });
-          return true;
-        }
-        if (notification is OverscrollNotification &&
-            notification.depth == 0 &&
-            notification.overscroll < 0) {
-          if (notification.metrics.axis == widget.direction) {
-            setState(() {
-              _currentExtent -= notification.overscroll;
-            });
-          }
-          return true;
-        }
-        if (notification is ScrollUpdateNotification &&
-            notification.depth == 0 &&
-            (notification.scrollDelta ?? 0) > 0 &&
-            _scrollable != null) {
-          if (notification.metrics.axis == widget.direction) {
-            setState(() {
-              _currentExtent = 0.0;
-              _scrolling = false;
-              _scrollable = null;
-            });
-            return true;
-          }
-        }
-        return false;
-      },
-      child: AnimatedValueBuilder.animation(
-        // value: _currentExtent,
-        value: _stage == TriggerStage.refreshing ||
-                _stage == TriggerStage.completed
-            ? widget.minExtent
-            : _currentExtent,
-        duration: _scrolling ? Duration.zero : kDefaultDuration,
-        curve: Curves.easeInOut,
-        builder: (context, animation) {
-          return Stack(
-            fit: StackFit.passthrough,
-            children: [
-              widget.child,
-              AnimatedBuilder(
-                animation: animation,
-                builder: (context, child) {
-                  return Positioned(
-                      top: 0,
-                      left: 0,
-                      right: 0,
-                      child: ClipRect(
-                        child: FractionalTranslation(
-                          translation: widget.direction == Axis.vertical
-                              ? const Offset(0, -1)
-                              : const Offset(-1, 0),
-                          child: Transform.translate(
-                            offset: widget.direction == Axis.vertical
-                                ? Offset(
-                                    0, _calculateSafeExtent(animation.value))
-                                : Offset(
-                                    _calculateSafeExtent(animation.value), 0),
-                            child: widget.indicatorBuilder(
-                              context,
-                              RefreshTriggerStage(
-                                _stage,
-                                animation,
-                                widget.direction,
+      onNotification: _handleScrollNotification,
+      child: NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: _handleOverscrollNotification,
+        child: AnimatedValueBuilder.animation(
+          // value: _currentExtent,
+          value: _stage == TriggerStage.refreshing ||
+                  _stage == TriggerStage.completed
+              ? widget.minExtent
+              : _currentExtent,
+          duration: _scrolling ? Duration.zero : kDefaultDuration,
+          curve: Curves.easeInOut,
+          builder: (context, animation) {
+            Tween<double> tween = Tween<double>(
+              begin: 0.0,
+              end: widget.minExtent,
+            );
+            return Stack(
+              fit: StackFit.passthrough,
+              children: [
+                widget.child,
+                AnimatedBuilder(
+                  animation: animation,
+                  builder: (context, child) {
+                    return Positioned.fill(
+                        child: ClipRect(
+                      child: Stack(
+                        children: [
+                          _wrapPositioned(
+                            FractionalTranslation(
+                              translation: _offset,
+                              child: Transform.translate(
+                                offset: widget.direction == Axis.vertical
+                                    ? Offset(0,
+                                        _calculateSafeExtent(animation.value))
+                                    : Offset(
+                                        _calculateSafeExtent(animation.value),
+                                        0),
+                                child: widget.indicatorBuilder(
+                                  context,
+                                  RefreshTriggerStage(
+                                    _stage,
+                                    tween.animate(animation),
+                                    widget.direction,
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ));
-                },
-              ),
-            ],
-          );
-        },
+                        ],
+                      ),
+                    ));
+                  },
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
