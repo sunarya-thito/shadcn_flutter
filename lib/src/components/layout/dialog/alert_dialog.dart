8b4a2532dfbbf47edb1913832823367983f6e5ea
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-10-26 12:44:07 +0700

    Update alert dialog to not compute intrinsic as possible

--- lib/src/components/layout/dialog/alert_dialog.dart
+++ lib/src/components/layout/dialog/alert_dialog.dart
@@ -31,62 +31,56 @@ class _AlertDialogState extends State<AlertDialog> {
   Widget build(BuildContext context) {
     var themeData = Theme.of(context);
     var scaling = themeData.scaling;
-    return IntrinsicWidth(
-      child: ModalContainer(
+    return ModalContainer(
+      borderRadius: themeData.borderRadiusXxl,
+      barrierColor: widget.barrierColor ?? Colors.black.withOpacity(0.8),
+      child: OutlinedContainer(
+        backgroundColor: themeData.colorScheme.popover,
         borderRadius: themeData.borderRadiusXxl,
-        barrierColor: widget.barrierColor ?? Colors.black.withOpacity(0.8),
-        child: OutlinedContainer(
-          backgroundColor: themeData.colorScheme.popover,
-          borderRadius: themeData.borderRadiusXxl,
-          borderWidth: 1 * scaling,
-          borderColor: themeData.colorScheme.muted,
-          padding: EdgeInsets.all(24 * scaling),
-          surfaceBlur: widget.surfaceBlur ?? themeData.surfaceBlur,
-          surfaceOpacity: widget.surfaceOpacity ?? themeData.surfaceOpacity,
-          child: IntrinsicHeight(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Flexible(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      if (widget.leading != null)
-                        widget.leading!.iconXLarge().iconMutedForeground(),
-                      if (widget.title != null || widget.content != null)
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              if (widget.title != null)
-                                widget.title!.large().semiBold(),
-                              if (widget.content != null)
-                                widget.content!.small().muted(),
-                            ],
-                          ).gap(8 * scaling),
-                        ),
-                      if (widget.trailing != null)
-                        widget.trailing!.iconXLarge().iconMutedForeground(),
-                    ],
-                  ).gap(16 * scaling),
-                ),
-                if (widget.actions != null && widget.actions!.isNotEmpty)
-                  IntrinsicHeight(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      // children: widget.actions!,
-                      children:
-                          join(widget.actions!, SizedBox(width: 8 * scaling))
-                              .toList(),
+        borderWidth: 1 * scaling,
+        borderColor: themeData.colorScheme.muted,
+        padding: EdgeInsets.all(24 * scaling),
+        surfaceBlur: widget.surfaceBlur ?? themeData.surfaceBlur,
+        surfaceOpacity: widget.surfaceOpacity ?? themeData.surfaceOpacity,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Flexible(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  if (widget.leading != null)
+                    widget.leading!.iconXLarge().iconMutedForeground(),
+                  if (widget.title != null || widget.content != null)
+                    Flexible(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (widget.title != null)
+                            widget.title!.large().semiBold(),
+                          if (widget.content != null)
+                            widget.content!.small().muted(),
+                        ],
+                      ).gap(8 * scaling),
                     ),
-                  ),
-              ],
-            ).gap(16 * scaling),
-          ),
-        ),
+                  if (widget.trailing != null)
+                    widget.trailing!.iconXLarge().iconMutedForeground(),
+                ],
+              ).gap(16 * scaling),
+            ),
+            if (widget.actions != null && widget.actions!.isNotEmpty)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                // children: widget.actions!,
+                children: join(widget.actions!, SizedBox(width: 8 * scaling))
+                    .toList(),
+              ),
+          ],
+        ).gap(16 * scaling),
       ),
     );
   }
