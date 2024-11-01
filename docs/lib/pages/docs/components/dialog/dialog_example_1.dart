ac543bb88456468a19f8db74a97f6b116dcff24b
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-11-01 18:24:04 +0700

    Fix dialog example

--- docs/lib/pages/docs/components/dialog/dialog_example_1.dart
+++ docs/lib/pages/docs/components/dialog/dialog_example_1.dart
@@ -20,25 +20,28 @@ class DialogExample1 extends StatelessWidget {
                   const Text(
                       'Make changes to your profile here. Click save when you\'re done'),
                   const Gap(16),
-                  Form(
-                    controller: controller,
-                    child: const FormTableLayout(rows: [
-                      FormField<String>(
-                        key: FormKey(#name),
-                        label: Text('Name'),
-                        child: TextField(
-                          initialValue: 'Thito Yalasatria Sunarya',
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 400),
+                    child: Form(
+                      controller: controller,
+                      child: const FormTableLayout(rows: [
+                        FormField<String>(
+                          key: FormKey(#name),
+                          label: Text('Name'),
+                          child: TextField(
+                            initialValue: 'Thito Yalasatria Sunarya',
+                          ),
                         ),
-                      ),
-                      FormField<String>(
-                        key: FormKey(#username),
-                        label: Text('Username'),
-                        child: TextField(
-                          initialValue: '@sunaryathito',
+                        FormField<String>(
+                          key: FormKey(#username),
+                          label: Text('Username'),
+                          child: TextField(
+                            initialValue: '@sunaryathito',
+                          ),
                         ),
-                      ),
-                    ]),
-                  ).withPadding(vertical: 16),
+                      ]),
+                    ).withPadding(vertical: 16),
+                  ),
                 ],
               ),
               actions: [
