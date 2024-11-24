8850a0bd3cb496968a1c981da698b272dd822151
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-11-24 09:20:49 +0700

    Update text field example 2

--- docs/lib/pages/docs/components/input/input_example_2.dart
+++ docs/lib/pages/docs/components/input/input_example_2.dart
@@ -1,13 +1,36 @@
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
-class InputExample2 extends StatelessWidget {
+class InputExample2 extends StatefulWidget {
   const InputExample2({super.key});
 
   @override
+  State<InputExample2> createState() => _InputExample2State();
+}
+
+class _InputExample2State extends State<InputExample2> {
+  final TextEditingController _searchController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return const TextField(
+    return TextField(
+      controller: _searchController,
       initialValue: 'Hello World!',
-      placeholder: Text('Enter your message'),
+      placeholder: Text('Search something...'),
+      leading: StatedWidget.builder(
+        builder: (context, states) {
+          if (states.focused) {
+            return Icon(Icons.search);
+          } else {
+            return Icon(Icons.search).iconMutedForeground();
+          }
+        },
+      ),
+      trailing: IconButton.text(
+        icon: Icon(Icons.close),
+        density: ButtonDensity.compact,
+        onPressed: () {
+          _searchController.clear();
+        },
+      ),
     );
   }
 }
