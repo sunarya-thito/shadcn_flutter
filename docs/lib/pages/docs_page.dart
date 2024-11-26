12487e13d5f0ffdbee9bb2429dc6e1f80aa7ec7c
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-11-26 21:24:49 +0700

    Updated table tag on docs

--- docs/lib/pages/docs_page.dart
+++ docs/lib/pages/docs_page.dart
@@ -296,7 +296,7 @@ class DocsPageState extends State<DocsPage> {
         // TODO also make it zoomable like: https://zoom-chart-demo.vercel.app/
         ShadcnDocsPage('Chart', 'chart', ShadcnFeatureTag.workInProgress),
         ShadcnDocsPage('Code Snippet', 'code_snippet'),
-        ShadcnDocsPage('Table', 'table'),
+        ShadcnDocsPage('Table', 'table', ShadcnFeatureTag.newFeature),
         ShadcnDocsPage('Tracker', 'tracker'),
       ],
     ),
