0d08eb91ac18285ff3e9b977980fc9e6377204fe
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2024-12-08 22:35:13 +0700

    Update input otp example

--- docs/lib/pages/docs/components/input_otp/input_otp_example_1.dart
+++ docs/lib/pages/docs/components/input_otp/input_otp_example_1.dart
@@ -1,19 +1,37 @@
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
-class InputOTPExample1 extends StatelessWidget {
+class InputOTPExample1 extends StatefulWidget {
   const InputOTPExample1({super.key});
 
   @override
+  State<InputOTPExample1> createState() => _InputOTPExample1State();
+}
+
+class _InputOTPExample1State extends State<InputOTPExample1> {
+  String value = '';
+  @override
   Widget build(BuildContext context) {
-    return InputOTP(
+    return Column(
+      mainAxisSize: MainAxisSize.min,
       children: [
-        InputOTPChild.character(allowDigit: true),
-        InputOTPChild.character(allowDigit: true),
-        InputOTPChild.character(allowDigit: true),
-        InputOTPChild.separator,
-        InputOTPChild.character(allowDigit: true),
-        InputOTPChild.character(allowDigit: true),
-        InputOTPChild.character(allowDigit: true),
+        InputOTP(
+          onChanged: (value) {
+            setState(() {
+              this.value = value.otpToString();
+            });
+          },
+          children: [
+            InputOTPChild.character(allowDigit: true),
+            InputOTPChild.character(allowDigit: true),
+            InputOTPChild.character(allowDigit: true),
+            InputOTPChild.separator,
+            InputOTPChild.character(allowDigit: true),
+            InputOTPChild.character(allowDigit: true),
+            InputOTPChild.character(allowDigit: true),
+          ],
+        ),
+        gap(16),
+        Text('Value: $value'),
       ],
     );
   }
