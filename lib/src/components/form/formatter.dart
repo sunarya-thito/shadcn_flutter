a33933837bb2c8c558183eabb2a132880762292b
Thito Yalasatria Sunarya <septogeddon@gmail.com>
2025-02-28 14:44:02 +0700

    Added datePart formatter

--- lib/src/components/form/formatter.dart
+++ lib/src/components/form/formatter.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/services.dart';
 
+import '../../../shadcn_flutter.dart';
+
 class TextInputFormatters {
   static const TextInputFormatter toUpperCase = _ToUpperCaseTextFormatter();
   static const TextInputFormatter toLowerCase = _ToLowerCaseTextFormatter();
   static const TextInputFormatter digitsOnly = _DigitsOnlyTextFormatter();
+  static TextInputFormatter datePart(Map<DatePart, int> values, DatePart part) {
+    return _DatePartFormatter(values, part);
+  }
+
   const TextInputFormatters._();
+}
+
+class _DatePartFormatter extends TextInputFormatter {
+  final Map<DatePart, int> values;
+  final DatePart part;
+
+  _DatePartFormatter(this.values, this.part);
+
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.isEmpty) {
+      return newValue;
+    }
+    final (int? min, int? max) = part.computeValueRange(values);
+    int? value = int.tryParse(newValue.text);
+    if (value == null) {
+      return oldValue;
+    }
+    if (min != null && value < min) {
+      value = min;
+    }
+    if (max != null && value > max) {
+      value = max;
+    }
+    return TextEditingValue(
+      text: value.toString(),
+      selection: newValue.selection,
+    );
+  }
 }
 
 class _DigitsOnlyTextFormatter extends TextInputFormatter {
